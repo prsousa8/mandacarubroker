1a61aa99dd1de9912ca9452aba24b8449e9374de
prsousa8 <prsousa.128@gmail.com>
2024-02-14 22:08:08 -0300

    test: create tests MandacarubrokerApplication

--- src/test/java/com/mandacarubroker/MandacarubrokerApplicationTests.java
+++ src/test/java/com/mandacarubroker/MandacarubrokerApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class MandacarubrokerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		MandacarubrokerApplication.main(new String[]{});
 	}
 
 }
