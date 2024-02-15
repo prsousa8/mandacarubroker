d62dfc46539d21f8bf7618333b4c92e7724d701c
prsousa8 <prsousa.128@gmail.com>
2024-02-15 09:42:52 -0300

    feat: add validation exception CampoVazioException.java

---
+++ src/main/java/com/mandacarubroker/service/exceptions/CampoVazioException.java
@@ -0,0 +1,7 @@
+package com.mandacarubroker.service.exceptions;
+
+public class CampoVazioException extends RuntimeException {
+    public CampoVazioException(String message) {
+        super(message);
+    }
+}
