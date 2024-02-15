08fcea1a09612e7ce3fc4b347f54a0dd1eaea12c
prsousa8 <prsousa.128@gmail.com>
2024-02-15 09:43:18 -0300

    feat: add validation exception RegistroNaoEncontradoException.java

---
+++ src/main/java/com/mandacarubroker/service/exceptions/RegistroNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.mandacarubroker.service.exceptions;
+
+public class RegistroNaoEncontradoException extends RuntimeException {
+    public RegistroNaoEncontradoException(String message) {
+        super(message);
+    }
+}
