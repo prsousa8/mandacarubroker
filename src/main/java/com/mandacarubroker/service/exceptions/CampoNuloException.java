37f7513fb702f1c4b04e76ef3b4a73a6866da48a
prsousa8 <prsousa.128@gmail.com>
2024-02-15 09:42:28 -0300

    feat: add validation exception CampoNuloException.java

---
+++ src/main/java/com/mandacarubroker/service/exceptions/CampoNuloException.java
@@ -0,0 +1,7 @@
+package com.mandacarubroker.service.exceptions;
+
+public class CampoNuloException extends RuntimeException {
+    public CampoNuloException(String message) {
+        super(message);
+    }
+}
