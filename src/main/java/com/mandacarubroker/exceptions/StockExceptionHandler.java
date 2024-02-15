e14e4ff2df52211a394419c6a469ae18098143b9
prsousa8 <prsousa.128@gmail.com>
2024-02-15 09:46:36 -0300

    Add exceptions StockExceptionHandler.java

--- src/main/java/com/mandacarubroker/exceptions/StockExceptionHandler.java
+++ src/main/java/com/mandacarubroker/exceptions/StockExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.mandacarubroker.exceptions;
 
+import com.mandacarubroker.service.exceptions.CampoNuloException;
+import com.mandacarubroker.service.exceptions.CampoVazioException;
 import com.mandacarubroker.service.exceptions.ObjectNotFoundException;
+import com.mandacarubroker.service.exceptions.RegistroNaoEncontradoException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,5 +20,20 @@ public class StockExceptionHandler {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         StandardError error = new StandardError(LocalDateTime.now().format(formatter), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(CampoVazioException.class)
+    public ResponseEntity<StandardError>CampoVazioException(CampoVazioException ex, HttpServletRequest request){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        StandardError error = new StandardError(LocalDateTime.now().format(formatter), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+
+    @ExceptionHandler(CampoNuloException.class)
+    public ResponseEntity<StandardError>CampoNuloException(CampoNuloException ex, HttpServletRequest request){
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    StandardError error = new StandardError(LocalDateTime.now().format(formatter), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 }
