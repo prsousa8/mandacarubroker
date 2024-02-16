8ec2e38b74d5f057a5010bbdb8f9c4bacd28c125
prsousa8 <prsousa.128@gmail.com>
2024-02-16 14:01:38 -0300

    Remove arquivo RegistroNaoEncontradoException.java

--- src/main/java/com/mandacarubroker/service/exceptions/RegistroNaoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.mandacarubroker.service.exceptions;
-
-public class RegistroNaoEncontradoException extends RuntimeException {
-    public RegistroNaoEncontradoException(String message) {
-        super(message);
-    }
-}
